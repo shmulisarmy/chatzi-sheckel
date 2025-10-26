618860d4656bfce8380d97d7df3f2f6537120828
shmuly keller <shmulikeller@gmail.com>
2025-10-26 12:30:07 +0000

    make the bricks fall to in the goal of creating an arch

--- src/components/page-components/building-animation.tsx
+++ src/components/page-components/building-animation.tsx
@@ -4,35 +4,17 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 import styles from './building-animation.module.css';
 
-const Brick = ({ className }: { className?: string }) => (
-  <div className={cn(styles.brick, className)} />
-);
-
-const FallingBrick = ({ id }: { id: string }) => (
-  <Brick className={cn(styles.fallingBrick, styles[id])} />
-);
-
-const BaseStructure = () => (
-  <div className={styles.baseStructure}>
-    <Brick className={styles.base1} />
-    <Brick className={styles.base2} />
-    <Brick className={styles.base3} />
-    <Brick className={styles.base4} />
-    <Brick className={styles.base5} />
-    <Brick className={styles.base6} />
-    <Brick className={styles.base7} />
-  </div>
+const AnimatedBrick = ({ id }: { id: string }) => (
+  <div className={cn(styles.brick, styles[id])} />
 );
 
 export const BuildingAnimation = () => {
   return (
     <div className={styles.animationContainer}>
-      <BaseStructure />
-      <FallingBrick id="falling1" />
-      <FallingBrick id="falling2" />
-      <FallingBrick id="falling3" />
-      <FallingBrick id="falling4" />
-      <FallingBrick id="falling5" />
+      {/* This renders 9 bricks and assigns them each a unique animation from the CSS module */}
+      {Array.from({ length: 9 }).map((_, i) => (
+        <AnimatedBrick key={i} id={`arch${i + 1}`} />
+      ))}
     </div>
   );
 };
