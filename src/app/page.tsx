89e7ca2efef38adc7076a6ac21067c97479e90d6
shmuly keller <shmulikeller@gmail.com>
2025-10-27 04:41:47 +0000

    you made the price and carosell section bigger instead of smaller, i wan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
       <Hero />
       <div className="container mx-auto px-4 py-8 -mt-16 md:-mt-32 relative z-10">
         <div className="flex flex-col-reverse lg:flex-row lg:space-x-12">
-          <div className="w-full lg:w-3/5 mt-8 lg:mt-0">
+          <div className="w-full lg:w-2/3 mt-8 lg:mt-0">
             <InformationalSections />
           </div>
-          <div className="w-full lg:w-2/5">
+          <div className="w-full lg:w-1/3">
             <div className="lg:sticky lg:top-24">
               <React.Suspense fallback={<div>Loading...</div>}>
                 <ProductCheckout />
