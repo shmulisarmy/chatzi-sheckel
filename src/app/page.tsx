4749c2892ab3fcd24bc96df9d2b00cedb71a7eae
shmuly keller <shmulikeller@gmail.com>
2025-10-27 07:45:51 +0000

    Don't just make the content on the left have more space around it rather

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,17 @@ export default function Home() {
       <Hero />
       <Header />
       <div className="container mx-auto px-4 py-8">
-        <div className="flex flex-col-reverse lg:flex-row lg:space-x-24">
-          <div className="w-full lg:w-3/4 mt-8 lg:mt-0">
-            <InformationalSections />
-          </div>
-          <div className="w-full lg:w-1/4">
-            <div className="lg:sticky lg:top-24 @container">
-              <React.Suspense fallback={<div>Loading...</div>}>
-                <ProductCheckout />
-              </React.Suspense>
+        <div className="max-w-7xl mx-auto">
+          <div className="flex flex-col-reverse lg:flex-row lg:space-x-24">
+            <div className="w-full lg:w-3/4 mt-8 lg:mt-0">
+              <InformationalSections />
+            </div>
+            <div className="w-full lg:w-1/4">
+              <div className="lg:sticky lg:top-24 @container">
+                <React.Suspense fallback={<div>Loading...</div>}>
+                  <ProductCheckout />
+                </React.Suspense>
+              </div>
             </div>
           </div>
         </div>
