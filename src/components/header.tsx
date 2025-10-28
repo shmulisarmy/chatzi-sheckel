554fe143f9795904b53c2d1173f4b96bc52107e0
shmuly keller <shmulikeller@gmail.com>
2025-10-28 03:25:06 +0000

    the same color that you use for the footer background use that for the b

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import { cn } from '@/lib/utils';
 export default function Header() {
   return (
     <header className={cn(
-      "sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+      "sticky top-0 z-40 w-full border-b border-secondary bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
     )}>
       <div className="container flex h-16 items-center sm:justify-between sm:space-x-0">
         <Link href="/" className="flex items-center space-x-2">
