d69205e8b3e06a15c12fd4dfbb8ebf22233aac45
shmuly keller <shmulikeller@gmail.com>
2025-11-17 00:22:04 +0000

    i see that on a browser (even one that has the light theme) the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+    <html lang="en" className="scroll-smooth">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
