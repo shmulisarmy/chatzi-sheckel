673d514710148a9aa76da860bf38f2c39db79cc3
shmulisamry <shmulikeller@gmail.com>
2025-11-23 08:14:29 -0500

    attached neon db url

--- src/app/on_server.ts
+++ src/app/on_server.ts
@@ -1,9 +1,11 @@
+// app/actions.ts
 "use server";
-import Database from "better-sqlite3";
+import { neon } from "@neondatabase/serverless";
 
-const db = new Database("questions.db");
-
-
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined");
+}
+const sql = neon(process.env.DATABASE_URL);
 
 
 
@@ -17,9 +19,7 @@ export type Question = {
 }
 
 export async function GetQuestions() {
-    const questions = db.prepare(`
-    select id, name, email, question, is_shown from questions
-`).all() as Question[];
-
+    const data = await sql`select id, name, email, question, is_shown from questions`;
+    const questions = data as Question[];
     return questions;
 }
