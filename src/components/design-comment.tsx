f6acab8bd42b8008f0540d7121a50ac0a2453bb3
shmulisamry <shmulikeller@gmail.com>
2025-11-23 10:23:31 -0500

    disabled using_design_comments

--- src/components/design-comment.tsx
+++ src/components/design-comment.tsx
@@ -12,9 +12,12 @@ export default function DesignComment() {
 
 
 const on_client = typeof window !== "undefined";    
+const config = {
+    using_design_comments: false
+}
 
 
-if (on_client) {
+if (on_client && config.using_design_comments) {
 window.addEventListener ("mouseover", (event) => {
     const element: HTMLElement = event.target;
     const prev_border = element.style.border;
