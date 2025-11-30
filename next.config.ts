2fad9d4699acb49fc3f1b8f807aedb74f590c7e7
shmuly keller <shmulikeller@gmail.com>
2025-11-30 01:59:13 +0000

    use this profile image of yossi keller instead

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.postimg.cc',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yoshkeller.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
