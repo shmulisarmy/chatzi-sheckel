0e898ccd69ac57d0f9a28009e92a5d8055d0294a
shmulisamry <shmulikeller@gmail.com>
2026-03-01 13:26:23 -0500

    added a product url redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 
 import type {NextConfig} from 'next';
 
+
+// next.config.js
+
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/product',
+        destination: 'https://r0ja9y-0g.myshopify.com/products/half-shekel-coin',
+        permanent: false, // 308 redirect
+      },
+    ]
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
